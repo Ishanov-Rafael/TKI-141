f8f805878221a5cc5d3598fb6026bf97b44414d6
Ишанов Рафаэль ТКИ-141 <rafaelishanov832@gmail.com>
2026-04-22 11:02:52 +0300

    Create color.h

---
+++ color.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <cstdint>
+
+/**
+ * @brief Класс Цвет в формате RGB
+ */
+class Color 
+{
+private:
+    uint8_t red;   
+    uint8_t green; 
+    uint8_t blue;  
+
+public:
+    /**
+     * @brief Конструктор с проверкой диапазона
+     * @param r красный
+     * @param g зеленый
+     * @param b синий
+     */
+    Color(int r = 0, int g = 0, int b = 0);
+
+    friend std::ostream& operator<<(std::ostream& os, const Color& c);
+    friend std::istream& operator>>(std::istream& is, Color& c);
+};
