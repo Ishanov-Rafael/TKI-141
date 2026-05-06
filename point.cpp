87d74acbf17050a222bfd64be16b5538ff5db1f9
Ишанов Рафаэль ТКИ-141 <rafaelishanov832@gmail.com>
2026-05-06 16:15:56 +0300

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -2,7 +2,13 @@
 
 bool Point::operator==(const Point& other) const 
 {
-    return (x == other.x && y == other.y);
+    const double eps = 1e-9;
+    return (std::abs(x - other.x)< eps && std::abs(y - other.y) < eps);
+}
+
+bool Point::operator!=(const Point& other) const
+{
+    return !(*this == other);
 }
 
 std::ostream& operator<<(std::ostream& os, const Point& p) 
